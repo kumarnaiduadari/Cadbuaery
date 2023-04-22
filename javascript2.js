578638069f02f7dde3ee7afa158ea10c47e53322
kumarnaiduadari <64322283+kumarnaiduadari@users.noreply.github.com>
2023-04-22 20:30:00 +0530

    Add files via upload

---
+++ javascript2.js
@@ -0,0 +1,23 @@
+function size1(){ 
+    var x=document.getElementById("ram").value;
+    document.getElementById("demo").innerHTML = x;
+}
+
+
+/*------------------javascript for alert--------------------*/
+              function alert3()
+                {
+                 alert("Your order is sucessfully placed!");
+                }
+                function alert1(){
+                    
+                    alert("your login sucessfull!");
+                    
+                }
+            
+            
+
+             
+             function alert2(){
+                 alert("your registration sucessfull!");
+                }
